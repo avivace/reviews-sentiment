8e77ef56b350262f8b2db8410d08b3813c06591b
Antonio Vivace <avivace4@gmail.com>
2019-11-25 00:33:11 +0100

    Extract gzipped datasets

--- datasets/pull_datasets.sh
+++ datasets/pull_datasets.sh
@@ -2,3 +2,7 @@
 wget http://deepyeti.ucsd.edu/jianmo/amazon/categoryFiles/Grocery_and_Gourmet_Food.json.gz
 wget http://deepyeti.ucsd.edu/jianmo/amazon/metaFiles/meta_Grocery_and_Gourmet_Food.json.gz
 wget http://deepyeti.ucsd.edu/jianmo/amazon/categoryFilesSmall/Grocery_and_Gourmet_Food_5.json.gz
+
+gunzip Grocery_and_Gourmet_Food.json.gz
+gunzip meta_Grocery_and_Gourmet_Food.json.gz
+gunzip Grocery_and_Gourmet_Food_5.json.gz
