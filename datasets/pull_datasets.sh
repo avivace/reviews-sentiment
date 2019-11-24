f30781019779f71b23a2d4b1771be7878138e931
Antonio Vivace <avivace4@gmail.com>
2019-11-25 00:28:50 +0100

    Create pull_datasets.sh

---
+++ datasets/pull_datasets.sh
@@ -0,0 +1,4 @@
+ #!/bin/bash
+wget http://deepyeti.ucsd.edu/jianmo/amazon/categoryFiles/Grocery_and_Gourmet_Food.json.gz
+wget http://deepyeti.ucsd.edu/jianmo/amazon/metaFiles/meta_Grocery_and_Gourmet_Food.json.gz
+wget http://deepyeti.ucsd.edu/jianmo/amazon/categoryFilesSmall/Grocery_and_Gourmet_Food_5.json.gz
