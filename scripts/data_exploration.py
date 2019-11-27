abcd51a7698047f53dda71e8d10530d0ba6eccdc
Matteo Coppola <mcoppola94@hotmail.it>
2019-11-28 00:31:02 +0100

    Done some work in exploration script

--- scripts/data_exploration.py
+++ scripts/data_exploration.py
@@ -11,27 +11,21 @@ sns.set_style("dark")
 from data_utils import *
 import os
 
-#%%
-'''
-with open('DatasetsPath.txt', 'r') as myfile:
-    pathfile = myfile.read()
-#pathfile = r'C:\Users\Matteo\University\Magistrale\Data Analytics\Progetto\reviews-sentiment\datasets'
-
-df = pd.read_csv(pathfile + '\Gourmet_food.csv', index_col=0)
-'''
-#%%
 # You must be in \reviews-sentiment folder
 os.chdir("..")
 
-#%%
 # Load dataset
 path = r'.\datasets\Grocery_and_Gourmet_Food_5.json'
 df = load_dataset(path)
-#%% 
-print("Exploratory_data_analysis")
-print("Number of reviews:", len(df))
 
-# Score Distribution
+#Create new feature "opinion" based on vote
+df = vote_to_opinion(df)
+
+print("DATA EXPLORATION")
+
+print("Number of records:", len(df))
+
+#Score Distribution
 ax = plt.axes()
 sns.countplot(df.overall, ax=ax)
 ax.set_title('Score Distribution')
@@ -39,27 +33,28 @@ plt.show()
 
 print("Average Score: ", np.mean(df.overall))
 print("Median Score: ", np.median(df.overall))
-#%%
 
-df = vote_to_opinion(df)
-# Score Distribution
+#Opinion Distribution
 ax = plt.axes()
 sns.countplot(df.opinion, ax=ax)
-ax.set_title('Score Distribution')
+ax.set_title('Opinion Distribution')
 plt.show()
 
 print("Proportion of positive review:", len(df[df.opinion == "positive"]) / len(df))
 print("Proportion of neutral review:", len(df[df.opinion == "neutral"]) / len(df))
 print("Proportion of negative review:", len(df[df.opinion == "negative"]) / len(df))
-#%% Stacked barplot (x-axis asin code, y-axis opinion)
-### TODO: barre con difetto da fixare
 
+
+'''#Stacked barplot (x-axis asin code, y-axis opinion)
+### TODO: qualcosa è sbagliato qui
 top_products = most_reviewed_products(df, 20)
 r = list(top_products['asin'].unique())
 positive = list(top_products.loc[top_products['opinion'] == 'positive', 'asin'].value_counts())
 neutral = list(top_products.loc[top_products['opinion'] == 'neutral', 'asin'].value_counts())
 negative = list(top_products.loc[top_products['opinion'] == 'negative', 'asin'].value_counts())
 raw_data = {'positive': positive, 'neutral': neutral, 'negative': negative}
+
+print("Opinions ",raw_data)
 
 totals = list(top_products['asin'].value_counts())
 positive_percentage = [i / j * 100 for i, j in zip(positive, totals)]
@@ -74,10 +69,9 @@ plt.bar(r, neutral_percentage, bottom=positive_percentage, color='#f9bc86', edge
 plt.bar(r, negative_percentage, bottom=[i + j for i, j in zip(positive_percentage, neutral_percentage)], color='#a3acff', edgecolor='white', width=bar_width)
 plt.xticks(r, names)
 plt.xlabel("group")
-plt.show()
+plt.show()'''
 
-#%% TODO 
-# Correlation between votes and opinion with a boxplot.
+'''# Correlation between votes and opinion with a boxplot.
 # Maybe a better representation than boxplot can be considered
 # This part of code DOESN'T work
 
@@ -86,7 +80,8 @@ plt.boxplot(x=df_significative_reviews['opinion'],
             y=pd.to_numeric(df_significative_reviews['vote'],
             errors='coerce'), 
             palette="Blues")
-plt.show()
+plt.show()'''
+
 
 
 
