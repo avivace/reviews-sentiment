21d217e0536a5922384f0f904ad094d6f39028ee
Antonio Vivace <avivace4@gmail.com>
2019-11-30 14:24:13 +0100

    Pull and extract only the 5-core dataset

--- datasets/pull_datasets.sh
+++ datasets/pull_datasets.sh
@@ -1,8 +1,11 @@
- #!/bin/bash
-wget http://deepyeti.ucsd.edu/jianmo/amazon/categoryFiles/Grocery_and_Gourmet_Food.json.gz
-wget http://deepyeti.ucsd.edu/jianmo/amazon/metaFiles/meta_Grocery_and_Gourmet_Food.json.gz
+#!/bin/bash
+# This script pulls the datasets and extract them
+# Data source: http://jmcauley.ucsd.edu/data/amazon/
+
+#wget http://deepyeti.ucsd.edu/jianmo/amazon/categoryFiles/Grocery_and_Gourmet_Food.json.gz
+#wget http://deepyeti.ucsd.edu/jianmo/amazon/metaFiles/meta_Grocery_and_Gourmet_Food.json.gz
 wget http://deepyeti.ucsd.edu/jianmo/amazon/categoryFilesSmall/Grocery_and_Gourmet_Food_5.json.gz
 
-gunzip Grocery_and_Gourmet_Food.json.gz
-gunzip meta_Grocery_and_Gourmet_Food.json.gz
+#gunzip Grocery_and_Gourmet_Food.json.gz
+#gunzip meta_Grocery_and_Gourmet_Food.json.gz
 gunzip Grocery_and_Gourmet_Food_5.json.gz
