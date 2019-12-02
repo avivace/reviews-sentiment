6797c5319fae95798ad287956d4bc97d1b7c2ccb
lucapalazzi <palazzi.luca18@gmail.com>
2019-12-02 16:50:26 +0100

    Built pipeline for topic sentiment

--- scripts/topic_sentiment_data_preparation.py
+++ scripts/topic_sentiment_data_preparation.py
@@ -1,4 +1,32 @@
 # -*- coding: utf-8 -*-
 
-def run(df):
-    pass
+from data_utils import preprocessing
+import gensim
+from gensim import models
+
+def preprocessing_reviews(df):
+    # Most frequent product 
+    product_id = df.asin.mode().iloc[0]
+    # Create a dataframe composed by the most reviewed product
+    df_product = df[df['asin'] == product_id]
+    reviews = df_product['reviewText'].tolist()
+    preprocessed_reviews = preprocessing(reviews)
+    return preprocessed_reviews
+
+
+def create_dictionary(df, preprocessed_reviews):
+    dictionary = gensim.corpora.Dictionary(preprocessed_reviews)
+    dictionary.filter_extremes(no_below=10) #, no_above=0.5)#, keep_n=100000)
+    return dictionary
+    
+
+def bag_of_words(df, preprocessed_reviews):
+    dictionary = create_dictionary(df)
+    bow_corpus = [dictionary.doc2bow(doc) for doc in preprocessed_reviews]
+    return bow_corpus
+    
+
+def tf_idf(df, bow_corpus):
+    tfidf = models.TfidfModel(bow_corpus)
+    corpus_tfidf = tfidf[bow_corpus]
+    return corpus_tfidf
