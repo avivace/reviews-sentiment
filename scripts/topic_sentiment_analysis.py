dd6bc064fdfdd35daff0309e0d6bf8270a5abe7a
lucapalazzi <palazzi.luca18@gmail.com>
2019-12-05 12:36:47 +0100

    Added topic coherence to find out the best number of topic for a specific product

--- scripts/topic_sentiment_analysis.py
+++ scripts/topic_sentiment_analysis.py
@@ -4,6 +4,7 @@
 import nltk
 import gensim
 nltk.download('wordnet')
+import matplotlib.pyplot as plt
 
 from topic_sentiment_data_preparation import bag_of_words, create_dictionary, tf_idf, preprocessing_reviews
 
@@ -14,18 +15,60 @@ def run(df):
     dictionary = create_dictionary(df, preprocessed_reviews)
     # LDA using Bag of Words
     bow_corpus = bag_of_words(df, preprocessed_reviews)
-    lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=10, id2word=dictionary, passes=2, workers=2)
+    lda_model = gensim.models.LdaMulticore(corpus=bow_corpus, num_topics=10, id2word=dictionary, passes=2, workers=2)
     for idx, topic in lda_model.print_topics(-1):
-        print('Topic: {} \nWords: {}'.format(idx, topic))
+        print('Topic: {} \nWords: {}'.format(idx, topic))    
+
+    
+    ### Topic coherence per trovare il miglior numero di topic
+    print('topic coherence\n\n\n')
+    limit = 10
+    coherence_bow = []
+    for n in range(1, limit):
+        lda_model = gensim.models.LdaMulticore(corpus=bow_corpus, num_topics=n, id2word=dictionary, passes=2, workers=2)
+        cm = gensim.models.ldamodel.CoherenceModel(model=lda_model, dictionary=dictionary, coherence='c_v', texts=preprocessed_reviews)
+        print(cm.get_coherence())
+        coherence_bow.append(cm.get_coherence())
+        print('TOPIC:', n)
+        for idx, topic in lda_model.print_topics(-1):
+            print('Topic: {} \nWords: {}'.format(idx, topic))    
+        
+    x = range(1, limit)
+    plt.plot(x, coherence_bow)
+    plt.xlabel("num_topics")
+    plt.ylabel("Coherence score")
+    plt.show()
+    
+    
     
     corpus_tfidf = tf_idf(df, bow_corpus)
     # LDA using Tf-Idf
-    lda_model_tfidf = gensim.models.LdaMulticore(corpus_tfidf, num_topics=10, id2word=dictionary, passes=2, workers=4)
+    lda_model_tfidf = gensim.models.LdaMulticore(corpus=corpus_tfidf, num_topics=10, id2word=dictionary, passes=2, workers=2)
     for idx, topic in lda_model_tfidf.print_topics(-1):
         print('Topic: {} Word: {}'.format(idx, topic))
+    
+        
+    print('topic coherence\n\n\n')
+    limit = 10
+    coherence_tfidf = []
+    for n in range(1, limit):
+        lda_model_tfidf = gensim.models.LdaMulticore(corpus=corpus_tfidf, num_topics=n, id2word=dictionary, passes=2, workers=4)
+        cm = gensim.models.ldamodel.CoherenceModel(model=lda_model_tfidf, dictionary=dictionary, coherence='c_v', texts=preprocessed_reviews)
+        print(cm.get_coherence())
+        coherence_tfidf.append(cm.get_coherence())
+        print('TOPIC:', n)
+        for idx, topic in lda_model_tfidf.print_topics(-1):
+            print('Topic: {} \nWords: {}'.format(idx, topic))    
+        
+    x = range(1, limit)
+    plt.plot(x, coherence_tfidf)
+    plt.xlabel("num_topics")
+    plt.ylabel("Coherence score")
+    plt.show()
+
+    
 
 '''
-
 #%% FERRI E BASSO
 def topic_based_tokenization(reviews):
     tokenizedReviews = {}
@@ -54,5 +97,4 @@ reviews = [reviews_values[i] for i in range(len(reviews_values))]
 #%%
 reviews_filtered = removing_stop_words(reviews)
 reviews_tokenized = topic_based_tokenization(reviews_filtered)
-
 '''
