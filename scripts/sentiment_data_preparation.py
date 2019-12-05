a97af6d09a17dff93d8260470d9b4c2404c1bee7
lucapalazzi <palazzi.luca18@gmail.com>
2019-12-05 17:17:32 +0100

    Moved wordcloud in utils script

--- scripts/sentiment_data_preparation.py
+++ scripts/sentiment_data_preparation.py
@@ -5,28 +5,9 @@ import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
-from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 #%%
-def wordcloud(data, title = None):
-    wordcloud = WordCloud(
-        background_color = 'white',
-        max_words = 200,
-        max_font_size = 40, 
-        scale = 3,
-        random_state = 42
-    ).generate(str(data))
-
-    fig = plt.figure(1, figsize = (20, 20))
-    plt.axis('off')
-    if title: 
-        fig.suptitle(title, fontsize = 20)
-        fig.subplots_adjust(top = 2.3)
-
-    plt.imshow(wordcloud)
-    plt.show()
-    
 
 def run(df):
     #Features selection
