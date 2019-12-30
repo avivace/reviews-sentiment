6e1dfb7364158ecf9275fdda69c677403056e42f
lucapalazzi <palazzi.luca18@gmail.com>
2019-12-30 13:06:00 +0100

    Aggiunti altri due grafici relativi ai top reviewers

--- scripts/data_exploration.py
+++ scripts/data_exploration.py
@@ -81,7 +81,7 @@ def run(df):
     '''
 
     #Stacked barplot (x-axis asin code, y-axis opinion)
-    fig, ax3 = plt.subplots()
+    fig, ax3 = plt.subplots(figsize=(15, 15))
     top_products = most_reviewed_products(df, 20)
     r = list(top_products['asin'].unique())
     positive = list(top_products.loc[top_products['opinion'] == 'positive', 'asin'].value_counts().reindex(top_products['asin'].unique(), fill_value=0))
@@ -100,20 +100,21 @@ def run(df):
 
     bar_width = 0.85
 
-    ax3.bar(r, positive_percentage, color='#b5ffb9', edgecolor='white', width=bar_width)
-    ax3.bar(r, neutral_percentage, bottom=positive_percentage, color='#f9bc86', edgecolor='white', width=bar_width)
-    ax3.bar(r, negative_percentage, bottom=[i + j for i, j in zip(positive_percentage, neutral_percentage)], color='#a3acff', edgecolor='white', width=bar_width)
+    ax3.bar(r, positive_percentage, color='#b5ffb9', edgecolor='white', width=bar_width, label='positive')
+    ax3.bar(r, neutral_percentage, bottom=positive_percentage, color='#f9bc86', edgecolor='white', width=bar_width, label='neutral')
+    ax3.bar(r, negative_percentage, bottom=[i + j for i, j in zip(positive_percentage, neutral_percentage)], color='#a3acff', edgecolor='white', width=bar_width, label='negative')
     #ax.xticks(r, names, rotation=90)
     ax3.set_xticklabels(r, rotation=90)
     ax3.set_xlabel('Code product')
-    ax3.legend()
+    legend = ax3.legend(loc='best', shadow=True, fontsize='large')
+    legend.get_frame().set_facecolor('#00FFCC')
     ax3.set_title('Opinion for besteller products')
     # plt.show()
     ax3.figure.savefig(r'./figures/1_sentiment_reviews_bestseller_products.svg', format='svg')
-    print("Exported 1_reviews.svg")
+    print("Exported 1_sentiment_reviews_besteller_products.svg")
 
     # Top 50 reviewers
-    fig, ax4 = plt.subplots()
+    fig, ax4 = plt.subplots(figsize=(15, 15))
     top_reviewers = most_active_reviewers(df, 50)
     '''
     top_reviewers = top_reviewers.groupby('reviewerID').agg({'overall':'mean',
@@ -123,8 +124,8 @@ def run(df):
     sns.countplot(top_reviewers.reviewerID, ax=ax4, order=top_reviewers['reviewerID'].value_counts().index)
     r = list(top_reviewers['reviewerID'].unique())
     ax4.set_xticklabels(r, rotation=90)
-    ax4.set_title('Top reviewers')
-    ax4.figure.savefig(r'./figures/1_reviewers.svg', format='svg')
+    ax4.set_title('Reviewers with most reviews')
+    ax4.figure.savefig(r'./figures/1_reviewers_most_reviews.svg', format='svg')
     '''
     r = list(top_reviewers['reviewerID'].unique())
     bar_width = 0.85
@@ -175,6 +176,71 @@ def run(df):
     for x in unverified3_head:
         print("Review: ", x)
         
+
+    fig, ax6 = plt.subplots(figsize=(15, 15))
+    top_reviewers = most_active_reviewers(df, 50)
+    r = list(top_reviewers['reviewerID'].unique())
+    positive = list(top_reviewers.loc[top_reviewers['opinion'] == 'positive', 'reviewerID'].value_counts().reindex(top_reviewers['reviewerID'].unique(), fill_value=0))
+    neutral = list(top_reviewers.loc[top_reviewers['opinion'] == 'neutral', 'reviewerID'].value_counts().reindex(top_reviewers['reviewerID'].unique(), fill_value=0))
+    negative = list(top_reviewers.loc[top_reviewers['opinion'] == 'negative', 'reviewerID'].value_counts().reindex(top_reviewers['reviewerID'].unique(), fill_value=0))
+    raw_data = {'positive': positive, 'neutral': neutral, 'negative': negative}
+    raw_data = pd.DataFrame(raw_data)
+
+    #print("Opinions ",raw_data)
     
+    totals = [i+j+k for i,j,k in zip(raw_data['positive'], raw_data['neutral'], raw_data['negative'])]
+    #totals = list(top_products['asin'].value_counts().reindex(top_products['asin'].unique(), fill_value=0))
+    positive_percentage = [i / j * 100 for i, j in zip(raw_data['positive'], totals)]
+    neutral_percentage = [i / j * 100 for i, j in zip(raw_data['neutral'], totals)]
+    negative_percentage = [i / j * 100 for i, j in zip(raw_data['negative'], totals)]
+
+    bar_width = 0.85
+
+    ax6.bar(r, positive_percentage, color='#b5ffb9', edgecolor='white', width=bar_width, label='positive')
+    ax6.bar(r, neutral_percentage, bottom=positive_percentage, color='#f9bc86', edgecolor='white', width=bar_width, label='neutral')
+    ax6.bar(r, negative_percentage, bottom=[i + j for i, j in zip(positive_percentage, neutral_percentage)], color='#a3acff', edgecolor='white', width=bar_width, label='negative')
+    #ax.xticks(r, names, rotation=90)
+    ax6.set_xticklabels(r, rotation=90)
+    ax6.set_xlabel('Reviewer ID')
+    legend = ax6.legend(loc='best', shadow=True, fontsize='large')
+    legend.get_frame().set_facecolor('#00FFCC')
+    ax6.set_title('Opinion of top reviewers')
+    plt.show()
+    ax6.figure.savefig(r'./figures/1_opinion_top_reviewers.svg', format='svg')
+    print("Exported 1_opinion_top_reviewers.svg")
+    
+    
+    fig, ax7 = plt.subplots(figsize=(15, 15))
+    #top_reviewers = most_active_reviewers(df, 50)
+    r = list(top_reviewers['reviewerID'].unique())
+    verified = list(top_reviewers.loc[top_reviewers['verified'] == True, 'reviewerID'].value_counts().reindex(top_reviewers['reviewerID'].unique(), fill_value=0))
+    unverified = list(top_reviewers.loc[top_reviewers['verified'] == False, 'reviewerID'].value_counts().reindex(top_reviewers['reviewerID'].unique(), fill_value=0))
+    #negative = list(top_reviewers.loc[top_reviewers['opinion'] == 'negative', 'reviewerID'].value_counts().reindex(top_reviewers['reviewerID'].unique(), fill_value=0))
+    raw_data = {'verified': verified, 'unverified': unverified}
+    raw_data = pd.DataFrame(raw_data)
+
+    #print("Opinions ",raw_data)
+    
+    totals = [i+j for i,j in zip(raw_data['verified'], raw_data['unverified'])]
+    #totals = list(top_products['asin'].value_counts().reindex(top_products['asin'].unique(), fill_value=0))
+    verified_percentage = [i / j * 100 for i, j in zip(raw_data['verified'], totals)]
+    unverified_percentage = [i / j * 100 for i, j in zip(raw_data['unverified'], totals)]
+    #negative_percentage = [i / j * 100 for i, j in zip(raw_data['negative'], totals)]
+
+    bar_width = 0.85
+
+    ax7.bar(r, verified_percentage, color='#b5ffb9', edgecolor='white', width=bar_width, label='verified')
+    ax7.bar(r, unverified_percentage, bottom=verified_percentage, color='#f9bc86', edgecolor='white', width=bar_width, label='unverified')
+    #ax6.bar(r, negative_percentage, bottom=[i + j for i, j in zip(positive_percentage, neutral_percentage)], color='#a3acff', edgecolor='white', width=bar_width)
+    #ax.xticks(r, names, rotation=90)
+    ax7.set_xticklabels(r, rotation=90)
+    ax7.set_xlabel('Reviewer ID')
+    legend = ax7.legend(loc='best', shadow=True, fontsize='large')
+    legend.get_frame().set_facecolor('#00FFCC')
+    #ax6.legend(loc='lower left', fontsize='large')
+    ax7.set_title('Verified vs Unverified reviews of top reviewers')
+    plt.show()
+    ax7.figure.savefig(r'./figures/1_verified_unverified.svg', format='svg')
+    print("Exported 1_verified_unverified.svg")
     # Ordinamento date
     #df = df.sort_values('month_year', ascending=True)
