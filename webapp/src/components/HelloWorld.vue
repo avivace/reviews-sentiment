268cfc6dafafc63a89f49451eb878df81315c044
Antonio Vivace <avivace4@gmail.com>
2020-01-09 14:48:19 +0100

    Finished LDA dynamically rendered layout and links

--- webapp/src/components/HelloWorld.vue
+++ webapp/src/components/HelloWorld.vue
@@ -17,196 +17,63 @@
   <v-flex xs12>
 
       <div class="my-2">
-        <v-btn color="primary" depressed @click="toggled=!toggled">LDA</v-btn>
-      </div>
-
-
-      <div class="my-2">
-        <v-btn color="primary" depressed>SENTIMENT ANALYSIS TEST</v-btn>
+        <v-btn color="primary" depressed @click="toggledlda=!toggledlda;toggledsent=false">LDA</v-btn> &nbsp;&nbsp;&nbsp;
+         <v-btn color="primary" depressed @click="toggledsent=!toggledsent;toggledlda=false">SENTIMENT ANALYSIS TEST</v-btn>
       </div>
 
 
   </v-flex>
-
-  <v-flex xs12 v-if="toggled">
-  <v-row>
-        <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-    ></v-img>
-
-    <v-card-title>
-      Top western road trips
-    </v-card-title>
-
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <v-btn
-        color="purple"
-        text
-      >
-        PYLAVDIS
-      </v-btn>
-
-    </v-card-actions>
-
-  </v-card>        
-
-  <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-    ></v-img>
-
-    <v-card-title>
-      Top western road trips
-    </v-card-title>
-
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <v-btn
-        color="purple"
-        text
-      >
-        PYLAVDIS
-      </v-btn>
-
-    </v-card-actions>
-
-  </v-card>
-  
-</v-row> <br>
-
- <v-row>
-
-    <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-    ></v-img>
-
-    <v-card-title>
-      Top western road trips
-    </v-card-title>
-
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <v-btn
-        color="purple"
-        text
-      >
-        PYLAVDIS
-      </v-btn>
-
-    </v-card-actions>
-
-  </v-card>        <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-    ></v-img>
-
-    <v-card-title>
-      Top western road trips
-    </v-card-title>
-
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <v-btn
-        color="purple"
-        text
-      >
-        PYLAVDIS
-      </v-btn>
-
-    </v-card-actions>
-
-  </v-card>
-  
-</v-row> <br> <v-row>
+  <v-flex xs12 v-if="toggledsent">
+    ecomi
+  </v-flex>
+  <v-flex xs12 v-if="toggledlda">
+  <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for="object in lda"
+          :key="object.nam"
+          cols=6
+        >
 
         <v-card
+        :key="object"
     class="mx-auto"
     max-width="344"
   >
     <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      :src="object.code+'.jpg'"
       height="200px"
     ></v-img>
 
     <v-card-title>
-      Top western road trips
+      {{ object.name }}
     </v-card-title>
 
     <v-card-subtitle>
-      1,000 miles of wonder
+      {{ object.description }}
     </v-card-subtitle>
 
     <v-card-actions>
       <v-btn
-        color="purple"
+
+        :href="'lda_'+object.code+'.html'"
+        target="_blank"
+        
         text
-      >
-        PYLAVDIS
+      >      pyLDAvis<v-icon right dark>mdi-open-in-new</v-icon>
+        
+ 
       </v-btn>
 
     </v-card-actions>
 
-  </v-card>        <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-    ></v-img>
+  </v-card>     
 
-    <v-card-title>
-      Top western road trips
-    </v-card-title>
 
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <v-btn
-        color="purple"
-        text
-      >
-        PYLAVDIS
-      </v-btn>
-
-    </v-card-actions>
-
-  </v-card>
+  </v-col>
   
 </v-row>
+</v-container>
       </v-flex>
 
     </v-layout>
@@ -218,7 +85,39 @@ export default {
   name: 'HelloWorld',
 
   data: () => ({
-    toggled: false,
+    toggledlda: false,
+    toggledsent: false,
+    lda: [
+    {
+      name:"TaoTronics Car Phone Mount Holder, Windshield /",
+      description:"Dashboard Universal Car Mobile Phone cradle for iOS / Android Smartphone and More",
+      code:"B00MXWFUQC",
+    },
+    {
+      name:"Samsung Qi Certified Wireless Charging Pad",
+      description:"with 2A Wall Charger- Supports wireless charging on Qi compatible smartphones.",
+      code:"B00UCZGS6S",
+    },
+    {
+      name:"Portable Charger Anker PowerCore 20100mAh",
+      description:"Ultra High Capacity Power Bank with 4.8A Output and PowerIQ Technology.",
+      code:"B00X5RV14Y",
+    },
+    {
+      name:"Anker 24W Dual USB Car Charger",
+      description:"PowerDrive 2 for iPhone X / 8/7 / 6s / Plus, iPad Pro/Air 2 / Mini, Note 5/4, LG, Nexus, HTC, and More",
+      code:"B00VH88CJ0",
+    },
+    {
+      name:"Anker PowerCore+ mini 3350mAh Lipstick-Sized Portable Charger",
+      description:"(3rd Generation, Premium Aluminum Power Bank) One of the Most Compact External Batteries",
+      code:"B005NF5NTK",
+    },
+    {
+      name:"Plantronics Voyager Legend Wireless Bluetooth Headset",
+      description:"Compatible with iPhone, Android, and Other Leading Smartphones - Black",
+      code:"B0092KJ9BU",
+    }],
     ecosystem: []
   }),
 };
