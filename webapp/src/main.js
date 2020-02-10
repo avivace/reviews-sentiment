529847ae4d984755515c33c592b9726981734997
Antonio Vivace <avivace4@gmail.com>
2020-02-10 17:52:34 +0100

    add apex charts

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import axios from 'axios'
+import VueApexCharts from 'vue-apexcharts'
 
 import '@mdi/font/css/materialdesignicons.css'
 import './stylus/main.styl'
@@ -10,6 +11,9 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
+Vue.use(VueApexCharts)
+
+Vue.component('apexchart', VueApexCharts)
 
 new Vue({
   vuetify,
